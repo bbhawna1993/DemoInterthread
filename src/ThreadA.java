cb9e9e3b9ca86f6964eaab1b1c2fdb30c3ce60d7
bhawna <bhawnabudhiraja1993@gmail.com>
2019-05-03 16:03:24 +0530

    added sleep for thread a

--- src/ThreadA.java
+++ src/ThreadA.java
@@ -9,6 +9,11 @@ public class ThreadA {
 		 * 3.synchronized(object name)=object name.wait()
 		 * 4.object on which you call wait will be same as synchronized block
 		 */
+		/*main thread sleeps so that child thread gets the chance to execute.
+	     because if it does not sleeps then scheduler may give the chance to
+	     main thread or to child thread to execute.
+		 */
+		Thread.sleep(10000);
 		synchronized(b)
 		{
 			System.out.println("main thread trying to call wait method");
